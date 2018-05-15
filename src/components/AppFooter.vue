384e29f7512401f60762c744eb2775c2afe06ed6
Hongbin Li <hblee12294@gmail.com>
2018-05-15 03:33:09 -0700

    Modified file structrue; Added AppFooter

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,90 @@
+<template>
+  <footer>
+      <div class="ft-item coop">
+        <button class="btn clr-bg-2">入驻下厨房市集</button>
+        <button class="btn clr-bg-2">广告合作</button>
+      </div>
+      <div class="ft-item info">
+        <div class="sitemap">
+          <a href="">美食生活杂志</a>
+          <a href="">厨房工作</a>
+          <a href="">帮助中心</a>
+          <a href="">联系我们</a>
+          <a href="">意见反馈</a>
+          <a href="">隐私声明</a>
+        </div>
+        <div class="copyright">
+          <div>Copyright © iachufang.com 京B2-20180499 <a href="http://www.miibeian.gov.cn">京ICP备13009078号-1</a> 京公网安备11010802022310号</div>
+          <div>违法和不良信息举报电话：13581683297 举报邮箱：talk@xiachufang.com</div>
+        </div>
+      </div>
+      <div class="ft-item social">
+        <div class="scl-icons">
+          <a href="https://weibo.com/xiachufang" target="_blank"><img src="@/assets/sinaweibo.svg" title="微博" alt="微博"></a>
+          <a href="https://twitter.com/xiachufang" target="_blank"><img src="@/assets/twitter.svg" title="推特" alt="推特"></a>
+        </div>
+        <div class="slogan">唯有美食与爱不可辜负</div>
+      </div>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter'
+}
+</script>
+
+<style>
+footer {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 10px 40px;
+  color: #909090;
+  font-size: 13px;
+}
+
+/* Cooperation */
+.coop {
+  padding: 15px 0;
+  flex: 0 0 100%;
+}
+
+.coop .btn {
+  margin-right: 20px;
+  font-size: 13px;
+}
+
+/* Information */
+.info {
+  flex: auto;
+}
+
+.info a {
+  color: #909090;
+}
+
+.info div {
+  padding: 3px 0;
+}
+
+/* Copyright */
+.copyright div {
+  padding: 3px 0;
+}
+
+/* Social Media */
+.social {
+  flex: auto;
+  text-align: right;
+}
+
+.scl-icons img {
+ height: 18px;
+ margin-left: 5px;
+}
+
+.social div {
+  padding: 3px 0;
+}
+
+</style>
---
+++ src/pages/App.vue
@@ -0,0 +1,134 @@
+<template>
+  <div id="app">
+    <header>
+      <div class="hd-item logo">
+        <img src="@/assets/logo.png" alt="logo">
+      </div>
+      <div class="hd-item search">
+        <input
+          class="ipt-search"
+          placeholder="搜索菜谱、食材"
+          v-model="searchText"><button class="btn clr-bg">搜菜谱</button>
+      </div>
+      <div class="hd-item menu">
+        <button class="btn-txt clr-txt">会员专区</button>
+        <button class="btn-txt clr-txt">菜谱分类</button>
+        <button class="btn-txt clr-txt">菜单</button>
+        <button class="btn-txt clr-txt">作品动态</button>
+      </div>
+      <div class="hd-item login">
+        <button class="btn-txt clr-txt">登陆</button>
+        <button class="btn-txt clr-txt">注册</button>
+      </div>
+    </header>
+    <!-- <router-view/> -->
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import AppFooter from '@/components/AppFooter'
+export default {
+  name: 'App',
+  components: {
+    AppFooter
+  },
+  data () {
+    return {
+      searchText: ''
+    }
+  }
+}
+</script>
+
+<style>
+/* Predefine
+------------------- */
+#app {
+  color: #202020;
+  font-family: sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
+a {
+  text-decoration: none;
+}
+
+/* Btn */
+.btn {
+  box-sizing: border-box;
+  padding: 13px 25px;
+  border: none;
+  background-color: #797979;
+  color: white;
+  vertical-align: middle;
+  cursor: pointer;
+}
+
+.btn-txt {
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+
+.clr-txt {
+  color: #dd3915;
+}
+
+.clr-bg {
+  background-color: #dd3915;
+}
+
+.clr-bg-2 {
+  background-color: #84cf6d;
+}
+
+/* Header
+--------------------- */
+header {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+  height: 70px;
+}
+
+.ipt-search {
+  box-sizing: border-box;
+  width: 300px;
+  height: 44px;
+  padding: 8px;
+  outline: none;
+  border: 1px solid #f3f3f3;
+  color: #222222;
+  font-size: 16px;
+  letter-spacing: 1px;
+  vertical-align: middle;
+  transition: .2s;
+}
+
+.ipt-search:focus {
+  border: 1px solid #909090;
+}
+
+.search .btn {
+  height: 44px;
+  padding: 0px 16px;
+  font-size: 16px;
+}
+
+.menu .btn-txt {
+  font-size: 16px;
+}
+
+.login .btn-txt {
+  font-size: 16px;
+}
+
+</style>
